974db8061a4185e0f9f7bfed3f5ac47c698c8c97
2dowon <dwon424@gmail.com>
2021-05-08 23:13:46 +0900

    1-2 function

---
+++ 1-types/1-2-function.ts
@@ -0,0 +1,67 @@
+{
+  // JavaScript 💩
+  function jsAdd(num1, num2) {
+    return num1 + num2;
+  }
+
+  // TypeScript ✨
+  function add(num1: number, num2: number): number {
+    return num1 + num2;
+  }
+
+  // JavaScript 💩
+  function jsFetchNum(id) {
+    // code ...
+    // code ...
+    // code ...
+    return new Promise((resolve, reject) => {
+      resolve(100);
+    });
+  }
+
+  // TypeScript ✨
+  function fetchNum(id: string): Promise<number> {
+    // code ...
+    // code ...
+    // code ...
+    return new Promise((resolve, reject) => {
+      resolve(100);
+    });
+  }
+
+  // JavaScript ✨ => TypeScript
+  // Optional parameter
+  function printName(firstName: string, lastName?: string) {
+    console.log(firstName);
+    console.log(lastName); // undefined
+  }
+  printName("Steve", "Jobs");
+  printName("Ellie");
+  printName("Anna");
+
+  // Default parameter
+  function printMessage(message: string = "default message") {
+    console.log(message);
+  }
+  printMessage();
+  printMessage("hi");
+
+  // Rest parameter
+  function addNumbers(...numbers: number[]): number {
+    return numbers.reduce((a, b) => a + b);
+  }
+  console.log(addNumbers(1, 2));
+  console.log(addNumbers(1, 2, 3, 4));
+  console.log(addNumbers(1, 2, 3, 4, 5, 0));
+
+  function addNums(...args: number[]): number {
+    let res = 0;
+    for (let i = 0; i < args.length; i++) {
+      res += args[i];
+    }
+    return res;
+  }
+  console.log(addNums(1, 2));
+  console.log(addNums(1, 2, 3, 4));
+  console.log(addNums(1, 2, 3, 4, 5, 0));
+}
